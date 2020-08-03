3df64f8deedf5ccec8149676c375940c94b123d1
franck dehornoy <franck.dehornoy@gmail.com>
2020-08-03 15:22:00 +0200

    1st Commit

---
+++ 00-main-base.cpp
@@ -0,0 +1,75 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window;
+
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        
+        std::cout << extensionCount << " extensions supported" << std::endl;
+    }
+
+    void mainLoop() {
+
+
+        glm::mat4 matrix;
+        glm::vec4 vec;
+        auto test = matrix * vec;
+
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ shaders/compile.bat
@@ -0,0 +1,3 @@
+c:\VulkanSDK\1.2.135.0\Bin32\glslc.exe shader.vert -o vert.spv
+c:\VulkanSDK\1.2.135.0\Bin32\glslc.exe shader.frag -o frag.spv
+pause
